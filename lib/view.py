e358c3889c87a9d1cf451c191c9d153a5a8ee1e0
Rocker-Mitchell <rocker.mat94@gmail.com>
2019-08-10 13:45:35 -0600

    Add better descriptions to View
    Want to not imply too much about how view is designed: subclasses should make their own print functions and use attributes to dynamically print content

--- lib/view.py
+++ lib/view.py
@@ -2,10 +2,17 @@
 
 
 class View:
-    """A base implementation for view subclasses."""
+    """A base implementation for view subclasses.
+
+    A print function isn't required here as views may have different contexts for printing (i.e. prompts for interactive
+    input, progress bars, debug messages) which should be implemented by those subclasses.
+    """
 
     def set_attr(self, name, value):
-        """Set an attribute on self for dependent print functions.
+        """Set an attribute on self.
+
+        Subclasses should use attributes for dynamic content to print. Errors should raise if an attribute was not set
+        or is the wrong type, but shouldn't error from the data itself.
 
         Args:
             name (str): The name for the attribute.
