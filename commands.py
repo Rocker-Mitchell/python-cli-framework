b61b21a419f86235c61cc18b6081e9f733f2e3ad
Rocker-Mitchell <rocker.mat94@gmail.com>
2019-08-27 19:36:48 -0600

    Relocate Directory to commands.py
    The use of this class was only in relation to commands, so it makes more sense that it belongs to the commands module than a generic lib package

--- commands.py
+++ commands.py
@@ -1,13 +1,73 @@
 """commands.py - Configuration of commands for cli.py."""
 
-# Imports
 
-import lib.directory as directory
+# Directory class
+
+class Directory:
+    """A container for directory information about commands."""
+
+    def __init__(self, command, help_str):
+        """Initialize object.
+
+        Args:
+            command (str): The name of the command.
+            help_str (str): The help string for the command.
+        """
+        self.command = command
+        self.help = help_str
+
+    def params_module_name(self):
+        """Get the command's param parser module name.
+
+        Returns:
+            str: The formatted name.
+        """
+        return self.command + 'params'
+
+    def params_class_name(self):
+        """Get the command's param parser class name.
+
+        Returns:
+            str: The formatted name.
+        """
+        return self.command.capitalize() + 'Params'
+
+    def controller_module_name(self):
+        """Get the command's controller module name.
+
+        Returns:
+            str: The formatted name.
+        """
+        return self.command + 'controller'
+
+    def controller_class_name(self):
+        """Get the command's controller class name.
+
+        Returns:
+            str: The formatted name.
+        """
+        return self.command.capitalize() + 'Controller'
+
+    def view_module_name(self):
+        """Get the command's view module name.
+
+        Returns:
+            str: The formatted name.
+        """
+        return self.command + 'view'
+
+    def view_class_name(self):
+        """Get the command's view class name.
+
+        Returns:
+            str: The formatted name.
+        """
+        return self.command.capitalize() + 'View'
 
 
-# Constants
+# Command Directory list
 
 DIRECTORIES = [
-    directory.Directory('example', 'example command')
+    Directory('example', 'example command')
 ]
 """Directory objects for configured commands."""
